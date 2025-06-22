eb04fb97758aceeb3f1d1a6d598bc41aeba5bd3e
Shunya <hrjp00@gmail.com>
2025-06-22 08:40:07 +0000

    Update README and configuration files; add setup script for dependencies

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # handy_mapping
+
+# setup
+```bash
+cd colcon_ws/src
+git clone https://github.com/hrjp/handy_mapping.git
+./handy_mapping/setup.sh
+```
+
+# rosbag record
+```bash
+ros2 launch handy_mapping sensor_view.launch.xml
+```
+```bash
+cd rosbag_dir
+ros2 bag record /velodyne_points /imu /tf_static
+```
+
+# generate map
+ros2 run glim_ros glim_rosbag rosbag2_2025_06_22-08_02_32_0.mcap --ros-args -p config_path:=/home/colcon_ws/src/handy_mapping/config/glim
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# glim
+curl -s https://koide3.github.io/ppa/setup_ppa.sh | sudo bash
+sudo apt update
+sudo apt install -y libiridescence-dev libboost-all-dev libglfw3-dev libmetis-dev
+sudo apt install -y libgtsam-points-dev
+#sudo apt install -y libgtsam-points-cuda12.6-dev
+sudo apt install -y ros-jazzy-glim-ros
+#sudo apt install -y ros-jazzy-glim-ros-cuda12.6
+
+# velodyne
+sudo apt install -y ros-humble-velodyne 
+
+# witmotion
+sudo apt-get install libqt5serialport5-dev
+cd "$SCRIPT_DIR/.."
+git clone -b ros2 --recursive https://github.com/ElettraSciComp/witmotion_IMU_ros.git witmotion_ros
+cd ..
+colcon build --symlink-install
+source install/setup.sh
